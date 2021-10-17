5d59274e92b97e43b699659fe5d2156b6dfc40b2
m.angelina <m.angellina.am@gmail.com>
2021-10-17 18:37:39 +0300

    alignment

---
+++ 1510/alignment.py
@@ -0,0 +1,20 @@
+def formatting(arr, position, n, mark):
+
+    text = ""
+    if position == "center":
+        position = "^"
+    elif position == "left":
+        position = "<"
+    elif position == "right":
+        position = ">"
+    for i in arr:
+            text += (("{0:" + mark + position + str(n) + "s}").format(i))
+            text += "\n"
+    return text
+
+arr = input("enter the text: ").split()
+position = input("center, left or right? ")
+n = int(input("enter the page size: "))
+mark = input("enter a mark: ")
+
+print(formatting(arr, position, n, mark))

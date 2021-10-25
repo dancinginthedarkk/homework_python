2694f0076cd00b8879f26e491b9e6d4f309e128f
m.angelina <m.angellina.am@gmail.com>
2021-10-25 17:45:00 +0300

    name

--- 0610/name.py
+++ 0610/name.py
@@ -5,17 +5,18 @@ def f():
     num = ''
 
     while name != 'q' and num != 'q':
-        name = input()
+        name = input('name: ')
         if name == 'q':
             break
-        num = input()
+        num = input('num: ')
         if num == 'q':
             break
-        if num[0] == '+' and num[1].isdigit \
+        if len(num) == 16:
+            num[0] == '+' and num[1].isdigit \
             and num[2] == "-" and num[3:6].isdigit \
             and num[6] == '-' and num[7:10].isdigit \
             and num[10] == '-' and num[11:13].isdigit \
-            and num[13] == '-' and num[14:] and len(num) == 16:
+            and num[13] == '-' and num[14:]
             slowar[name] = num
         else:
             print('Wrong!')
